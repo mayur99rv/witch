d3165588b39776b5c6ae31400efc7808a9e3023f
mayur99rv <66148617+mayur99rv@users.noreply.github.com>
2021-02-20 01:18:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ gsap.registerPlugin(TextPlugin);
 var tl = gsap.timeline({scrollTrigger:{
     trigger:'.svg-container',
     start:'0% 0%',
-    end:'+=350vh 0%',
+    end:'+=4500 0%',
     pin:'.svg-container',
 //     pinSpacing:true,
     scrub:0.65,
